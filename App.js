c59e14acba9e01b675f26bde7359cf90fb11c086
PayalGandhile <payalgandhile80@gmail.com>
2026-04-06 23:15:43 +0530

    Learn the Component in React

--- App.js
+++ App.js
@@ -1,35 +1,80 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxHeading = (
-  <h3 id="heading" className="head">
-    Namsate React using the JSX
-  </h3>
-);
+/**
+ * Header
+ *    -Logo
+ *    -Nav Item
+ * Body
+ *    -Search
+ *    -ReastaurantContainer
+ *        -ReastaurantCard
+ *          -Image
+ *          - Name of Reastaurant, Star Rating, Cuisine, 
+ * Footer
+ *    -copyright
+ *    -links
+ *    -Address
+ *    -contact
 
-//React functional component
-// The function that return jsx called the functional component.
-// both function are the same 
+ */
 
-// const HeadingComponent = () => {
-//   return <h1 className="Heading"> This is the functional component with return keyword</h1>
-// }
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://png.pngtree.com/png-vector/20250217/ourmid/pngtree-unique-food-logo-png-image_15488394.png" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
+const ReastaurantCard = () => {
+  return (
+    <div className="restro-card">
+      <img alt="res-logo" className="res-log" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/8/7/d4c64e0f-946e-4c65-8be3-b6b8bd711244_b370d7b7-337a-471c-9f1e-a7b6a1ac245d.JPG" />
+      <div className="res-content">
+        <h3 className="res-name">Golden Wing Family Restaurant</h3>
+        <p className="res-cuisine">Biryani Chakan</p>
+        <div className="res-details ">
+          <span className="rating">4.1 (512 ratings)</span>
+          <span className="time">45-50 mins</span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-const Title = () => (<h1>let render it in the HeadingComponent2</h1>);
-const HeadingComponent2 = () => (
-  <div className="container">
-    {/* reactCompnet in the component or way to write it*/}
-    <Title />
-    <Title></Title>
-    {Title()};
-    <h1 className="Heading"> This is the functional component without return keyword</h1>
-  </div>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="ReastaurantContainer">
+        <ReastaurantCard />
+        <ReastaurantCard />
 
+      </div>
+    </div>
 
+  );
+}
+
+const AppLayout = () => {
+  return <div className="app">
+    <Header />
+    <Body />
+    {/* <Footer/> */}
+  </div>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent2 />)
-// root.render(jsxHeading);
+root.render(< AppLayout />)
+
